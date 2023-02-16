c3c01ed4ca132ec4327cdde4c89b69b1798583ea
masete <nicholasmasete72@gmail.com>
2023-02-16 10:41:25 +0300

    productionizing model

--- Initial model/API/funding/digest/apps.py
+++ Initial model/API/funding/digest/apps.py
@@ -1,6 +1,16 @@
+
 from django.apps import AppConfig
+import html
+import pathlib
+import os
 
+from digest.prediction import BertClassificationPredictor
 
-class DigestConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "digest"
+class WebappConfig(AppConfig):
+    name = 'digest'
+    MODEL_PATH = Path("model")
+    BERT_PRETRAINED_PATH = Path("model/uncased_L-12_H-768_A-12/")
+    LABEL_PATH = Path("label/")
+    predictor = BertClassificationPredictor(model_path = MODEL_PATH/"multilabel-emotion-fastbert-basic.bin", 
+                                            pretrained_path = BERT_PRETRAINED_PATH, 
+                                            label_path = LABEL_PATH, multi_label=True)  
